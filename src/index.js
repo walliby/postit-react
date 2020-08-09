4e73522c30df823c883b3612011a42fec9402b91
Josh Wallace <walliby84@gmail.com>
2020-08-08 21:11:40 -0400

    stub out bootstrap layout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class PostItBoard extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    render() {
+        return (
+            <div>
+                <h1>Posts</h1>
+                <div className="card mb-3">
+                    <div className="card-body">
+                        <h5 className="card-title">Post title</h5>
+                        <p>Post goes here</p>
+                    </div>
+                    <div className="card-footer">
+                        Comments go here
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <PostItBoard/>,
     document.getElementById('root')
 );
