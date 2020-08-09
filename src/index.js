7a1471d1b4915ace7e6de362db8d1ea1aa3ffc4a
Josh Wallace <walliby84@gmail.com>
2020-08-09 09:53:18 -0400

    add pagination

--- src/index.js
+++ src/index.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import Post from './Post'
+import Post from './Post';
 
 class PostItBoard extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            error: null,
             posts: [],
-            error: null
-        }
+            page: 1,
+            totalCount: 0
+        };
+
+        this.prevButtonRef = React.createRef();
+        this.nextButtonRef = React.createRef();
+        this.fetchNextPage = this.fetchNextPage.bind(this);
+        this.fetchPreviousPage = this.fetchPreviousPage.bind(this);
     }
 
-    componentDidMount() {
-        const endPoint = `https://jsonplaceholder.typicode.com/posts?_embed=comments&_expand=user`;
+    endPoint(page) {
+        return `https://jsonplaceholder.typicode.com/posts
+            ?_page=${page}
+            &_embed=comments
+            &_expand=user`
+            .replace(/\s/g, '');
+    }
 
-        fetch(endPoint)
-            .then(res => res.json())
+    fetchPosts(page) {
+        let totalCount;
+
+        fetch(this.endPoint(page))
+            .then(result => {
+                totalCount = parseInt(result.headers.get('x-total-count'));
+                return result.json()
+            })
             .then(
                 result => {
-                    this.setState({posts: result});
+                    this.setState({
+                        posts: result,
+                        page: page,
+                        totalCount: totalCount
+                    });
                 },
                 error => {
                     this.setState({error});
@@ -28,23 +50,79 @@ class PostItBoard extends React.Component {
             )
     }
 
+    componentDidMount() {
+        this.fetchPosts(1);
+    }
+
+    fetchNextPage() {
+        this.fetchPosts(this.state.page + 1);
+    }
+
+    fetchPreviousPage() {
+        this.fetchPosts(this.state.page - 1);
+    }
+
+    disableNextButton() {
+        return this.state.page === Math.ceil(this.state.totalCount / 10);
+    }
+
+    disablePrevButton() {
+        return this.state.page === 1;
+    }
+
     render() {
-        const posts = this.state.posts.map(post => {
-            return (
-                <Post key={post.id} post={post}/>
+        let body;
+
+        if (this.state.error) {
+            body = <div>Error: {this.state.error.message}</div>;
+        } else {
+            const posts = this.state.posts.map(post => {
+                return <Post key={post.id} post={post}></Post>;
+            });
+
+            body = (
+                <ul className="list-group" role="region" aria-live="polite">
+                    {posts}
+                </ul>
             );
-        });
+        }
+
+        if (this.disablePrevButton() && this.nextButtonRef.current !== null) {
+            this.nextButtonRef.current.focus();
+        }
+
+        if (this.disableNextButton() && this.prevButtonRef.current !== null) {
+            this.prevButtonRef.current.focus();
+        }
 
         return (
             <div>
                 <h1>Posts</h1>
-                {posts}
+                <div className="d-flex justify-content-between mb-2">
+                    <button
+                        onClick={this.fetchPreviousPage}
+                        ref={this.prevButtonRef}
+                        type="button"
+                        className="btn btn-info"
+                        aria-label="Previous records"
+                        disabled={this.disablePrevButton()}
+                    >Prev</button>
+                    <button
+                        onClick={this.fetchNextPage}
+                        ref={this.nextButtonRef}
+                        type="button"
+                        className="btn btn-info"
+                        aria-label="Next records"
+                        disabled={this.disableNextButton()}
+                    >Next</button>
+                </div>
+                {body}
             </div>
         );
     }
 }
 
 ReactDOM.render(
-    <PostItBoard/>,
+    <PostItBoard />,
     document.getElementById('root')
 );
