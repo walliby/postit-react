a73114362ebc39aee76160eb1d6f24b6b889857f
Josh Wallace <walliby84@gmail.com>
2020-08-08 21:56:59 -0400

    add user address

--- src/post.js
+++ src/post.js
@@ -9,6 +9,18 @@ function Comment(props) {
     );
 }
 
+function Address(props) {
+    return(
+        <address role="region" aria-live="polite">
+            {props.user.address.street} {props.user.address.suite}<br/>
+            {props.user.address.city}<br/>
+            {props.user.address.zipcode}<br/>
+            <a href={`tel:+${props.user.phone}`}>{props.user.phone}</a><br/>
+            <a href={`mailto:+${props.user.email}`}>{props.user.email}</a>
+        </address>
+    );
+}
+
 export default function Post(props) {
     const comments = props.post.comments.map(comment => {
         return <Comment key={comment.id} comment={comment}/>;
@@ -18,6 +30,8 @@ export default function Post(props) {
         <div className="card mb-3">
             <div className="card-body">
                 <h5 className="card-title">{props.post.title}</h5>
+                <strong>{props.post.user.name}</strong>
+                <Address user={props.post.user}/>
                 <p>{props.post.body}</p>
             </div>
             <div className="card-footer">
