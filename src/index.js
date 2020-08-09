2cfa016a831fa6b36d6a236f822f6d4bb8fe9e2e
Josh Wallace <walliby84@gmail.com>
2020-08-08 21:21:26 -0400

    fetch posts

--- src/index.js
+++ src/index.js
@@ -3,23 +3,47 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class PostItBoard extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: [],
+            error: null
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(
+                result => {
+                    this.setState({posts: result});
+                },
+                error => {
+                    this.setState({error});
+                }
+            )
+    }
 
     render() {
-        return (
-            <div>
-                <h1>Posts</h1>
-                <div className="card mb-3">
+        const posts = this.state.posts.map(post => {
+            return (
+                <div key={post.id} className="card mb-3">
                     <div className="card-body">
-                        <h5 className="card-title">Post title</h5>
-                        <p>Post goes here</p>
+                        <h5 className="card-title">{post.title}</h5>
+                        <p>{post.body}</p>
                     </div>
                     <div className="card-footer">
                         Comments go here
                     </div>
                 </div>
+            );
+        });
+
+        return (
+            <div>
+                <h1>Posts</h1>
+                {posts}
             </div>
         );
     }
